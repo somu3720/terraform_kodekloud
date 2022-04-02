314819cba57634fea6844b5195ff25fef047f474
Somnath Pandey <somu3720@gmail.com>
2022-04-02 17:14:35 +0530

    Update main2.tf

--- main2.tf
+++ main2.tf
@@ -15,10 +15,24 @@ variable "instance_type" {
 resource "aws_instance" "cerberus" {
   ami = var.ami
   instance_type = var.instance_type
-
+  key_name = "cerberus"
+  user_data = file("./install-nginx.sh")
 }
 
+#You can also use variable for key_name
 
+resource "aws_key_pair" "cerberus-key" {
+  key_name = "cerberus"
+  public_key = file(".ssh/cerberus.pub")
+}
+
+resource "aws_eip" "eip" {
+  vpc = true
+  instance = aws_instance.cerberus.id
+  provisioner "local-exec" {
+      command = "echo${aws_eip.eip.public_dns} >> /root/cerberus_public_dns.txt"
+  }
+}
 
 .............................................
   
